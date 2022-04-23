fc7ddc08dd465a2e2f978cb9285acfeeaee77e5d
ThomasDCW <thomas.dicola@hotmail.fr>
2022-04-23 09:59:37 +0200

    reset style on html, create Homepage components and all looks good

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import Globalstyle from "./style";
+import Homepage from "@components/Homepage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Globalstyle>
+        Trop cool La vie !
+        <Homepage />
+      </Globalstyle>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Homepage/index.jsx
@@ -0,0 +1,9 @@
+import SHompepage from "./style";
+
+export default function Homepage() {
+  return (
+    <div>
+      <SHompepage>Ouai j'avoue !</SHompepage>
+    </div>
+  );
+}
