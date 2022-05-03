7dcc3acc1a6d64d4825366ac8d81bc5b771cb00a
ThomasDCW <thomas.dicola@hotmail.fr>
2022-05-03 22:00:33 +0200

    add list style decimal

--- frontend/src/components/Apipokemon/style.jsx
+++ frontend/src/components/Apipokemon/style.jsx
@@ -3,15 +3,14 @@ import styled from "styled-components";
 export default styled.ul`
   display: flex;
   flex-wrap: wrap;
+  text-align: center;
+  color: white;
+  font-size: 16px;
   li {
+    list-style: decimal-leading-zero;
     margin: 4em;
-    p {
-      font-size: 32px;
-      text-align: center;
-      color: white;
-    }
-    img {
-      width: 10em;
-    }
+  }
+  img {
+    width: 10em;
   }
 `;
