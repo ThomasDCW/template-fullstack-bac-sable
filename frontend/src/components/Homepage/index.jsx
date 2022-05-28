1c632db016a37dfda264abab85dd30bbe352116e
ThomasDCW <thomas.dicola@hotmail.fr>
2022-05-28 07:27:17 +0200

    delete burgermenu

--- frontend/src/components/Homepage/index.jsx
+++ frontend/src/components/Homepage/index.jsx
@@ -2,9 +2,5 @@ import SHompepage from "./style";
 import BurgerNav from "@components/BurgerNav";
 
 export default function Homepage() {
-  return (
-    <SHompepage>
-      <BurgerNav />
-    </SHompepage>
-  );
+  return <SHompepage></SHompepage>;
 }
